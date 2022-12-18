4cc1e6ce1055077a078219bbe5379dbbc7abfc6d
Ihor Neizvestnyi <neizvestnyi.igor@gmail.com>
2022-12-18 22:04:26 +0200

    03

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Hello world!');
+const burgerIcon = document.querySelector('.burger-icon');
+const burgerMenu = document.querySelector('.burger-menu');
+const burgerClose = document.querySelector('.burger-close');
+const body = document.body;
+
+if (burgerIcon) {
+	burgerIcon.addEventListener('click', function () {
+		burgerMenu.classList.add('active');
+		body.classList.add('scroll');
+	});
+}
+
+if (burgerClose) {
+	burgerClose.addEventListener('click', function () {
+		burgerMenu.classList.remove('active');
+		body.classList.remove('scroll');
+	});
+}
